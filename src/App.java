39ddf3090ccc49cf74ef452aa42f7edda0bde7eb
andrewklundtFT <ak1036178@student.francistuttle.edu>
2023-09-13 14:35:58 -0500

    All workingg but displaying factors

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in); 
+        boolean runnin = true;
+        String input;
+        int inputAsInt;
+        while (runnin) {
+            System.out.println("gimme integer");
+            input = scan.nextLine();
+            if (input.equals("exit")) {
+                runnin = false;
+                break;
+            }
+            try {
+                inputAsInt = Integer.parseInt(input);
+                displayFactors(inputAsInt);
+            } catch (Exception e)
+            {
+                System.out.println("put just the number like a digit like the key on your keyboard like 6 not like six or like 6! or anything else just the number that's all i want");
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public static void displayFactors (int x) {
+        System.out.println("farty");
     }
 }
